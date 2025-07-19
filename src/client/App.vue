263cce3c99f60eaff56302627ee8c2851b1170fb
Desmond Rivet <desmond.rivet@gmail.com>
2025-07-18 20:50:47 -0400

    shorter tic tac tow grid

--- src/client/App.vue
+++ src/client/App.vue
@@ -32,20 +32,10 @@ function cellClick(event: MouseEvent) {
 <template>
   <table>
     <tbody>
-      <tr>
-        <td class="cell" data-id="0" @click="cellClick">{{ stateRef?.G.cells[0] }}</td>
-        <td class="cell" data-id="1" @click="cellClick">{{ stateRef?.G.cells[1] }}</td>
-        <td class="cell" data-id="2" @click="cellClick">{{ stateRef?.G.cells[2] }}</td>
-      </tr>
-      <tr>
-        <td class="cell" data-id="3" @click="cellClick">{{ stateRef?.G.cells[3] }}</td>
-        <td class="cell" data-id="4" @click="cellClick">{{ stateRef?.G.cells[4] }}</td>
-        <td class="cell" data-id="5" @click="cellClick">{{ stateRef?.G.cells[5] }}</td>
-      </tr> 
-      <tr>
-        <td class="cell" data-id="6" @click="cellClick">{{ stateRef?.G.cells[6] }}</td>
-        <td class="cell" data-id="7" @click="cellClick">{{ stateRef?.G.cells[7] }}</td>
-        <td class="cell" data-id="8" @click="cellClick">{{ stateRef?.G.cells[8] }}</td>
+      <tr v-for="r in 3">
+        <td v-for="d in 3" class="cell" v-bind:data-id="(r - 1) * 3 + (d - 1)" @click="cellClick">
+          {{ stateRef?.G.cells[(r - 1) * 3 + (d - 1)] }}
+        </td>
       </tr>
     </tbody>
   </table>
